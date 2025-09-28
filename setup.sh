43a72a7b24d71717bd63ff60d000c39d71b3250f
WahyuAndikaRahadi <wahyuandikarahadi19@gmail.com>
2025-09-28 20:22:37 +0700

    update requirements

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@
 # Perbarui daftar paket
 apt-get update
 
-# Instal Python 3.10, developernya, dan distutils
-apt-get install -y python3.10 python3.10-dev python3.10-distutils
+# Instal Python 3.9 dan dependensinya
+# Python 3.9 masih menyertakan distutils sebagai modul terpisah
+apt-get install -y python3.9 python3.9-dev python3.9-distutils
 
-# Atur Python 3.10 sebagai default 'python3'
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+# Atur Python 3.9 sebagai default 'python3'
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
 
 echo "Setup complete. Proceeding with pip installation."
